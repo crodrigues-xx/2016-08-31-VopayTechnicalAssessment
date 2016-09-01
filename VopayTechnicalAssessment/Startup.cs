2b4a877d66d8219c084e36c0dc3ab845b20056e9
clive <cliverodrigues@gmail.com>
2016-09-01 15:40:11 -0700

    Vopay Technical Assessment 1 and 2

---
+++ VopayTechnicalAssessment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VopayTechnicalAssessment.Startup))]
+namespace VopayTechnicalAssessment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
